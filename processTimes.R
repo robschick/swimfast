0d229ea83d594c2ea126c1e98b2d12a7e8441122
Rob Schick <rss5@st-andrews.ac.uk>
2016-06-28 15:56:36 +0100

    Get times in - data wise, and formatted properly

---
+++ processTimes.R
@@ -0,0 +1,13 @@
+library(readr)
+library(stringr)
+library(lubridate)
+stimes <- read_csv('data/stellaTimes.csv', col_types = 'ccDccc')
+distance <- as.integer(noquote(str_replace(str_split_fixed(stimes$Race, ' ', 2)[, 1], 'm', '')))
+stroke <- str_split_fixed(stimes$Race, ' ', 2)[, 2]
+stimes$Stroke <- stroke
+stimes$Distance <- distance
+
+stimes$newTimes <- as.difftime(stimes$Time, format = '%M:%OS')
+# http://stackoverflow.com/questions/14480575/how-to-work-with-times-distance-and-speed
+# Try Edelbuettel's suggestion:
+
